2c1e32339dca5a7236d40b2c45204457850d3cc8
Paolo Cetti <cettipao@live.com.ar>
2020-10-09 11:30:35 -0300

    eventos/urls.py creado, flyerView iniciada, jet agregado a requirements.txt

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import *
+from django.urls import path, include
 
 urlpatterns = [
     path('^jet/', include('jet.urls', 'jet')),
     path('admin/', admin.site.urls),
+    path('', include('eventos.urls')),
 ]
