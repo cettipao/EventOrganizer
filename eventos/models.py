063a638ce3ff194aac677a9594a632e761ae6af3
Lucio <LucioMorales>
2020-10-09 11:11:32 -0300

    Creacion Models del Invitado

--- eventos/models.py
+++ eventos/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Invitado(models.Model):
+    numero = models.CharField(max_length=30)
+    nombre = models.CharField(max_length=30, null=True, blank=True)
+    sexos = [
+        ("H", 'Hombre'),
+        ("M", 'Mujer'),
+    ]
+    sexo = models.CharField(
+        max_length=2,
+        choices=sexos,
+        blank=True,
+    )
+    confirmado = models.BooleanField(default = False)
+    cambio_nombre = models.BooleanField(default=False)
+
+    def str(self):
+        return "{}, {}".format(self.numero, self.nombre)
