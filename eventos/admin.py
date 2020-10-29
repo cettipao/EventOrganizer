bd9d37567dc3b27862126c8387fdc0dd01b4d1ad
Lucio <LucioMorales>
2020-10-29 11:24:53 -0300

    Models Ingreso, Mesa creados, Cambios en Admin

--- eventos/admin.py
+++ eventos/admin.py
@@ -4,16 +4,33 @@ from .models import *
 # Register your models here.
 
 class InvitadoAdmin(admin.ModelAdmin):
-    list_display = ['numero','nombre','sexo','confirmado']    
-    list_display_links = ['numero','nombre','sexo','confirmado']
+    list_display = ['numero','nombre','sexo','mesa']
+    list_display_links = ['numero','nombre','sexo','mesa']
+    search_fields = ['nombre', 'numero']
+    list_filter = ('sexo', 'mesa',)
 
     fieldsets = (
         ('Datos', {
-          'fields': ('numero','nombre','sexo')  
+          'fields': ('numero','nombre','sexo')
         }),
         ('Extra', {
-            'fields': ('confirmado','cambio_nombre',)
+            'fields': ('mesa',)
         }),
     )
+    actions = ['make_hombre','make_mujer']
+    def make_hombre(self, request, queryset):
+        return queryset.update(sexo="H")
+
+    def make_mujer(self, request, queryset):
+        return queryset.update(sexo="M")
+
+class InvitadoInLine(admin.TabularInline): #Para ver los prestamos de la persona
+    model = Invitado
+
+class MesaAdmin(admin.ModelAdmin):
+    inlines = [InvitadoInLine, ]
+    list_display = ('numero_mesa', 'isInfantil')
 
 admin.site.register(Invitado,InvitadoAdmin)
+admin.site.register(Mesa, MesaAdmin)
+admin.site.register(Ingreso)
--- eventos/models.py
+++ eventos/models.py
@@ -2,6 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+
+class Mesa(models.Model):
+    numero_mesa = models.IntegerField()
+    infantil = models.BooleanField()
+
+    def isInfantil(self):
+        return self.infantil
+    isInfantil.boolean = infantil
+    isInfantil.short_description = 'Mesa de Niños'
+
+    def str(self):
+        return str(self.numero_mesa)
+
 class Invitado(models.Model):
     numero = models.CharField(max_length=30)
     nombre = models.CharField(max_length=30, null=True, blank=True)
@@ -14,8 +27,25 @@ class Invitado(models.Model):
         choices=sexos,
         blank=True,
     )
+    mesa = models.ForeignKey(
+        'Mesa',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
     confirmado = models.BooleanField(default = False)
     cambio_nombre = models.BooleanField(default=False)
 
-    def __str__(self):
+    def str(self):
         return "{}, {}".format(self.numero, self.nombre)
+
+class Ingreso(models.Model):
+    invitado = models.OneToOneField(
+        'Invitado',
+        on_delete=models.CASCADE,
+        null=False
+    )
+    hora = models.DateTimeField(auto_now=True)
+
+    def str(self):
+        return "{}, {}".format(self.invitado.nombre, self.hora.ctime())
